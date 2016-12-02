1df4f1187a6adfc56536f69efebd759d6b10af6b
Jaz Lalli <jazlalli@hotmail.com>
2016-12-02 13:28:36 +0000

    Tidy up

--- index.js
+++ index.js
@@ -1,44 +1,7 @@
 // NOTHING TO SEE HERE
 // go look inside the folders
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 import Queue from './Queue';
 import Tree from './Tree';
-
-import menuData from './data.json';
-import Item from './components/Item';
-
-const selectedItems = new Map();
-
-const onSelected = (id, quantity) => {
-  selectedItems.set(id, quantity);
-}
-
-window.showBasket = function () {
-  selectedItems.forEach((val, key) => {
-    console.log('item:', key, val);
-  });
-}
-
-const displayItems = menuData.items
-  .map(item => (
-    <Item
-      key={item.id}
-      item={item}
-      leaf={(item.modifier_groups.length === 0)}
-      selectedId={item.id}
-      onSelected={onSelected}
-    />
-  ));
-
-const App = (props) => (
-  <div>
-    {displayItems}
-  </div>
-);
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // var q = new Queue();
 // for (var i=0; i<10; i++) {
